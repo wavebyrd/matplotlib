wavebyrd/matplotlib

commit 747c4ec1b5df31637c1544c18c908d3dd24e7542

MNT: raise exception if generators passed in
 - moved next(iter(obj)) logic into a cbook function

lib/matplotlib/cbook.py
@@ -12,6 +12,7 @@ from __future__ import (absolute_import, division, print_function,
 from matplotlib.externals import six
 from matplotlib.externals.six.moves import xrange, zip
 from itertools import repeat
+import collections
 
 import datetime
 import errno
@@ -2534,6 +2535,13 @@ def index_of(y):
     except AttributeError:
         y = np.atleast_1d(y)
         return np.arange(y.shape[0], dtype=float), y
+
+
+def safe_first_element(obj):
+    if isinstance(obj, collections.Iterator):
+        raise RuntimeError("matplotlib does not support generators "
+                           "as input")
+    return next(iter(obj))
 
 
 def get_label(y, default_name):

lib/matplotlib/axes/_axes.py
@@ -2331,11 +2331,11 @@ class Axes(_AxesBase):
         """
         # process the unit information
         if len(xranges):
-            xdata = next(iter(xranges))
+            xdata = cbook.safe_first_element(xranges)
         else:
             xdata = None
         if len(yrange):
-            ydata = next(iter(yrange))
+            ydata = cbook.safe_first_element(yrange)
         else:
             ydata = None
         self._process_unit_info(xdata=xdata,
@@ -5886,7 +5886,8 @@ class Axes(_AxesBase):
                 Name to use in ValueError if `inp` can not be normalized
 
             """
-            if isinstance(x, np.ndarray) or not iterable(next(iter(inp))):
+            if (isinstance(x, np.ndarray) or
+                    not iterable(cbook.safe_first_element(inp))):
                 # TODO: support masked arrays;
                 inp = np.asarray(inp)
                 if inp.ndim == 2:

lib/matplotlib/dates.py
@@ -1561,7 +1561,7 @@ class DateConverter(units.ConversionInterface):
             x = x.ravel()
 
         try:
-            x = next(iter(x))
+            x = cbook.safe_first_element(x)
         except (TypeError, StopIteration):
             pass
 

commit f83738603744f8f42fafe8ed49af9f3596cf0fb0

FIX: support np.flatiter as input again
This was unintentionally broken by
747c4ec1b5df31637c1544c18c908d3dd24e7542 which went in as part of
PR #5556 which was fixing support for pandas series.

lib/matplotlib/cbook.py
@@ -2544,6 +2544,15 @@ def index_of(y):
 
 def safe_first_element(obj):
     if isinstance(obj, collections.Iterator):
+        # needed to accept `array.flat` as input.
+        # np.flatiter reports as an instance of collections.Iterator
+        # but can still be indexed via [].
+        # This has the side effect of re-setting the iterator, but
+        # that is acceptable.
+        try:
+            return obj[0]
+        except TypeError:
+            pass
         raise RuntimeError("matplotlib does not support generators "
                            "as input")
     return next(iter(obj))

lib/matplotlib/tests/test_cbook.py
@@ -499,3 +499,15 @@ def test_grouper_private():
     base_set = mapping[ref(objs[0])]
     for o in objs[1:]:
         assert mapping[ref(o)] is base_set
+
+
+def test_flatiter():
+    x = np.arange(5)
+    it = x.flat
+    assert 0 == next(it)
+    assert 1 == next(it)
+    ret = cbook.safe_first_element(it)
+    assert ret == 0
+
+    assert 0 == next(it)
+    assert 1 == next(it)