wavebyrd/matplotlib

commit db59ded74efbe7c010f893c81fae1bb3243f565e

Give a better error message on missing PostScript fonts
For #4167; does not fix the problem, as it would need supporting
a whole different kind of font, but gives a more useful error
message.

lib/matplotlib/backends/backend_pdf.py
@@ -1876,6 +1876,12 @@ class RendererPdf(RendererBase):
                 pdfname = self.file.fontName(dvifont.texname)
                 if dvifont.texname not in self.file.dviFontInfo:
                     psfont = self.tex_font_mapping(dvifont.texname)
+                    if psfont.filename is None:
+                        self.file.broken = True
+                        raise ValueError(
+                            ("No usable font file found for %s (%s). "
+                             "The font may lack a Type-1 version.")
+                            % (psfont.psname, dvifont.texname))
                     self.file.dviFontInfo[dvifont.texname] = Bunch(
                         fontfile=psfont.filename,
                         basefont=psfont.psname,

lib/matplotlib/tests/test_backend_pdf.py
@@ -7,13 +7,21 @@ from matplotlib.externals import six
 
 import io
 import os
+import tempfile
+
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+from nose.tools import raises
 
 import numpy as np
-from matplotlib import cm, rcParams
+from matplotlib import checkdep_tex, cm, rcParams
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib import pyplot as plt
 from matplotlib.testing.decorators import (image_comparison, knownfailureif,
                                            cleanup)
+from matplotlib import dviread
 
 if 'TRAVIS' not in os.environ:
     @image_comparison(baseline_images=['pdf_use14corefonts'],
@@ -28,6 +36,10 @@ if 'TRAVIS' not in os.environ:
         text = '''A three-line text positioned just above a blue line
     and containing some French characters and the euro symbol:
     "Merci pépé pour les 10 €"'''
+
+needs_tex = knownfailureif(
+    not checkdep_tex(),
+    "This test needs a TeX installation")
 
 
 @cleanup
@@ -132,3 +144,19 @@ def test_grayscale_alpha():
     ax.imshow(dd, interpolation='none', cmap='gray_r')
     ax.set_xticks([])
     ax.set_yticks([])
+
+
+@cleanup
+@needs_tex
+@raises(ValueError)
+@patch('matplotlib.dviread.PsfontsMap.__getitem__')
+def test_missing_psfont(mock):
+    """An error is raised if a TeX font lacks a Type-1 equivalent"""
+    psfont = dviread.PsFont(texname='texfont', psname='Some Font',
+                            effects=None, encoding=None, filename=None)
+    mock.configure_mock(return_value=psfont)
+    rcParams['text.usetex'] = True
+    fig, ax = plt.subplots()
+    ax.text(0.5, 0.5, 'hello')
+    with tempfile.TemporaryFile() as tmpfile:
+        fig.savefig(tmpfile, format='pdf')

lib/matplotlib/tests/test_backend_svg.py
@@ -5,12 +5,22 @@ from matplotlib.externals import six
 
 import numpy as np
 from io import BytesIO
+import os
+import tempfile
 import xml.parsers.expat
+
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+from nose.tools import raises
 
 import matplotlib.pyplot as plt
 from matplotlib.testing.decorators import cleanup
 from matplotlib.testing.decorators import image_comparison, knownfailureif
 import matplotlib
+from matplotlib import dviread
+
 
 needs_tex = knownfailureif(
     not matplotlib.checkdep_tex(),
@@ -181,6 +191,23 @@ def test_determinism_notex():
 def test_determinism_tex():
     # unique filename to allow for parallel testing
     _test_determinism('determinism_tex.svg', usetex=True)
+
+
+@cleanup
+@needs_tex
+@raises(ValueError)
+@patch('matplotlib.dviread.PsfontsMap.__getitem__')
+def test_missing_psfont(mock):
+    """An error is raised if a TeX font lacks a Type-1 equivalent"""
+    from matplotlib import rc
+    psfont = dviread.PsFont(texname='texfont', psname='Some Font',
+                            effects=None, encoding=None, filename=None)
+    mock.configure_mock(return_value=psfont)
+    rc('text', usetex=True)
+    fig, ax = plt.subplots()
+    ax.text(0.5, 0.5, 'hello')
+    with tempfile.NamedTemporaryFile(suffix='.svg') as tmpfile:
+        fig.savefig(tmpfile.name)
 
 
 if __name__ == '__main__':

lib/matplotlib/textpath.py
@@ -335,6 +335,12 @@ class TextToPath(object):
             font_bunch = self.tex_font_map[dvifont.texname]
 
             if font_and_encoding is None:
+                if font_bunch.filename is None:
+                    raise ValueError(
+                        ("No usable font file found for %s (%s). "
+                         "The font may lack a Type-1 version.")
+                        % (font_bunch.psname, dvifont.texname))
+
                 font = get_font(font_bunch.filename)
 
                 for charmap_name, charmap_code in [("ADOBE_CUSTOM",

commit 7c321d27b9ab4c28d2a708cdaa2d350127fd6719

Remove cutout for missing font file in PdfFile._embedTeXFont.
If fontfile is None, an error would already have been raised earlier in
dviFontName (which explicitly checks for this case).

(The cutout in _embedTeXFont was introduced first, in ed0066f (2009),
but support for that case appears to have been broken a bit later and
the check in dviFontName was then introduced in db59ded (2016) where it
simply improved the clarity of the exception ultimately raised.)

lib/matplotlib/backends/backend_pdf.py
@@ -1021,18 +1021,6 @@ class PdfFile:
                     0, *map(Name, dviread._parse_enc(fontinfo.encodingfile))],
             }
 
-        # If no file is specified, stop short
-        if fontinfo.fontfile is None:
-            _log.warning(
-                "Because of TeX configuration (pdftex.map, see updmap option "
-                "pdftexDownloadBase14) the font %s is not embedded. This is "
-                "deprecated as of PDF 1.5 and it may cause the consumer "
-                "application to show something that was not intended.",
-                fontinfo.basefont)
-            fontdict['BaseFont'] = Name(fontinfo.basefont)
-            self.writeObject(fontdictObject, fontdict)
-            return fontdictObject
-
         # We have a font file to embed - read it in and apply any effects
         t1font = _type1font.Type1Font(fontinfo.fontfile)
         if fontinfo.effects: