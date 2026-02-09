be81a50df07e9427d462ea2024c03d93258343d7
Leon Merten Lohse <llohse@uni-goettingen.de>
2026-02-09 10:30:07 +0100

    mathtext: implement \mathnormal and distinguish between normal and it
    To replicate LaTeX behaviour, distinguish between "italic" and "normal"
    math. In particular, digits should be set italic in the \mathit env.
    
    For `cm`, use cmti font for "it"
    For general UnicodeFont (stix, DejaVu, ...), maps digits to roman or
    italic alphabet, depending on "normal" or "it" environment.

--- lib/matplotlib/font_manager.py
+++ lib/matplotlib/font_manager.py
@@ -1134,7 +1134,7 @@ class FontManager:
     # Increment this version number whenever the font cache data
     # format or behavior has changed and requires an existing font
     # cache files to be rebuilt.
-    __version__ = '3.11.0a2'
+    __version__ = '3.11.0a3'
 
     def __init__(self, size=None, weight='normal'):
         self._version = self.__version__
